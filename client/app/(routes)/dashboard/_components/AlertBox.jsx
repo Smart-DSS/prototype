30e682d048701250f42d32f4b859001a7ada6247
nisheethnayan <nisheethnayan.nn03@gmail.com>
2024-06-29 02:19:24 +0530

    procedure complete and made responsive and added ScrollingAlert

--- client/app/(routes)/dashboard/_components/AlertBox.jsx
+++ client/app/(routes)/dashboard/_components/AlertBox.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const AlertBox = () => {
   return (
+  <>
     <div className="flex justify-center h-[200px] w-[90%] bg-[#FF00001A] border-t-[10px] border-[#FF000080] rounded-3xl animate-fadeInUp">
+      
       <div className="flex flex-col justify-center font-mono font-extrabold text-md md:text-2xl">
         No Alerts At The Moment
       </div>
     </div>
+  </>
   );
 };
 
