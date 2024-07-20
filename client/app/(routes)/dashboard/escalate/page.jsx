edcf0e5b7629cf670ac719b55c247352578817bb
Joshua <joshu2@MacBook-Air.local>
2024-07-20 12:40:33 +0530

    added escalate card

---
+++ client/app/(routes)/dashboard/escalate/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div
+      className="h-screen pt-[5%]"
+      style={{
+        backgroundImage: "url('/background.svg')",
+      }}
+    >
+      <div className="bg-white/90 rounded-2xl w-[90%] h-[90%] mx-auto p-[5%] ">
+        escalate table
+      </div>
+    </div>
+  );
+};
+
+export default page;
