edf47b613d6e33e8f9532cfb0ab09c7b2489dade
Joshua <joshu2@MacBook-Air.local>
2024-07-23 11:48:41 +0530

    escalate and confiramtion page created

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     images:{
         domains:["lh3.googleusercontent.com"]
-    }
+    },
+    webpack: config => {
+        config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, tls:false};
+        return config;
+      },
 };
 
 export default nextConfig;
